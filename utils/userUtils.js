f51fbf35849ae717fedf613d427c228cc992c43a
BorislavBranimirov <boriintorr@gmail.com>
2021-06-19 00:59:28 +0100

    made exports syntax consistent

--- utils/userUtils.js
+++ utils/userUtils.js
@@ -1,11 +1,11 @@
 const db = require('../db/db');
 
-module.exports.usernamePatternTest = (username) => {
+exports.usernamePatternTest = (username) => {
   const usernamePattern = /^[a-zA-Z0-9]{6,25}$/;
   return usernamePattern.test(username);
 };
 
-module.exports.passwordPatternTest = (password) => {
+exports.passwordPatternTest = (password) => {
   const passwordPattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,72}$/;
   return passwordPattern.test(password);
 };
