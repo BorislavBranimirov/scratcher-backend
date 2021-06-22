daccc54274d36dcb1d395cf78775e6d4de5341db
BorislavBranimirov <boriintorr@gmail.com>
2021-06-22 14:33:54 +0100

    separated express app and server

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const helmet = require('helmet');
+
+const app = express();
+
+require('dotenv').config();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use(morgan('tiny'));
+
+app.use(helmet());
+
+const { authRouter, userRouter, scratchRouter } = require('./routes');
+app.use('/api/auth', authRouter);
+app.use('/api/users', userRouter);
+app.use('/api/scratches', scratchRouter);
+
+module.exports = app;
