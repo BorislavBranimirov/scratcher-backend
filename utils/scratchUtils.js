0e32533b9c391cd235cea842d17ab712a521d373
BorislavBranimirov <boriintorr@gmail.com>
2021-07-19 01:35:51 +0100

    extracted scratch utils into separate functions

--- utils/scratchUtils.js
+++ utils/scratchUtils.js
@@ -12,77 +12,108 @@ exports.getAdditionalScratchData = async (scratch, loggedUserId) => {
     isBookmarked: false
   };
 
-  // author
-  obj.author = await db('users')
-    .select('id', 'name', 'username', 'profileImageUrl')
-    .where({ id: scratch.authorId })
-    .first();
+  obj.author = await getAuthor(scratch.authorId);
 
-  // rescratched scratch
   if (scratch.rescratchedId) {
-    obj.rescratch = await db('scratches')
-      .select('*')
-      .where({ id: scratch.rescratchedId })
-      .first();
-
-    obj.rescratch.author = await db('users')
-      .select('id', 'name', 'username', 'profileImageUrl')
-      .where({ id: obj.rescratch.authorId })
-      .first();
+    obj.rescratch = await getRescratch(scratch, loggedUserId);
   }
 
-  // counters
-  obj.replyCount = (await db('scratches')
-    .count('*')
-    .where({ parentId: scratch.id }))[0].count;
-
-  obj.rescratchCount = (await db('scratches')
-    .count('*')
-    .where({ rescratchedId: scratch.id }))[0].count;
-
-  obj.likeCount = (await db('likes')
-    .count('*')
-    .where({ scratchId: scratch.id }))[0].count;
-
-  // statuses
-  if (loggedUserId) {
-    // posts by the logged user that have shared the scratch
-    const userRescratches = await db('scratches')
-      .select('*')
-      .where({
-        authorId: loggedUserId,
-        rescratchedId: scratch.id
-      });
-    // status only applies to direct rescratches, with no text body or media
-    for (const userRescratch of userRescratches) {
-      if (!userRescratch.body && !userRescratch.mediaUrl) {
-        obj.isRescratched = true;
-        break;
-      }
-    }
-
-    const userLike = await db('likes')
-      .select('*')
-      .where({
-        userId: loggedUserId,
-        scratchId: scratch.id
-      })
-      .first();
-    if (userLike) {
-      obj.isLiked = true;
-    }
-
-    const userBookmark = await db('bookmarks')
-      .select('*')
-      .where({
-        userId: loggedUserId,
-        scratchId: scratch.id
-      })
-      .first();
-    if (userBookmark) {
-      obj.isBookmarked = true;
-    }
-  }
+  Object.assign(
+    obj,
+    await getCounters(scratch.id),
+    await getStatuses(scratch.id, loggedUserId)
+  );
 
   return obj;
+};
+
+const getAuthor = async (id) => {
+  return await db('users')
+    .select('id', 'name', 'username', 'profileImageUrl')
+    .where({ id })
+    .first();
+};
+
+const getRescratch = async (scratch, loggedUserId) => {
+  const rescratch = await db('scratches')
+    .select('*')
+    .where({ id: scratch.rescratchedId })
+    .first();
+
+  rescratch.author = await getAuthor(rescratch.authorId);
+
+  return rescratch;
+};
+
+const getCounters = async (id) => {
+  const counters = {
+    replyCount: 0,
+    rescratchCount: 0,
+    likeCount: 0
+  };
+
+  counters.replyCount = (await db('scratches')
+    .count('*')
+    .where({ parentId: id }))[0].count;
+
+  counters.rescratchCount = (await db('scratches')
+    .count('*')
+    .where({ rescratchedId: id }))[0].count;
+
+  counters.likeCount = (await db('likes')
+    .count('*')
+    .where({ scratchId: id }))[0].count;
+
+  return counters;
+};
+
+const getStatuses = async (id, loggedUserId) => {
+  const statuses = {
+    isRescratched: false,
+    isLiked: false,
+    isBookmarked: false
+  };
+
+  if (!loggedUserId) {
+    return statuses;
+  }
+
+  // posts by the logged user that have shared the scratch
+  const userRescratches = await db('scratches')
+    .select('*')
+    .where({
+      authorId: loggedUserId,
+      rescratchedId: id
+    });
+  // status only applies to direct rescratches, with no text body or media
+  for (const userRescratch of userRescratches) {
+    if (!userRescratch.body && !userRescratch.mediaUrl) {
+      statuses.isRescratched = true;
+      break;
+    }
+  }
+
+  const userLike = await db('likes')
+    .select('*')
+    .where({
+      userId: loggedUserId,
+      scratchId: id
+    })
+    .first();
+  if (userLike) {
+    statuses.isLiked = true;
+  }
+
+  const userBookmark = await db('bookmarks')
+    .select('*')
+    .where({
+      userId: loggedUserId,
+      scratchId: id
+    })
+    .first();
+  if (userBookmark) {
+    statuses.isBookmarked = true;
+  }
+
+  return statuses;
 };
